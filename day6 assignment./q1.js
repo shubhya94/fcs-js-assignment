a200160c95a1a7c1f5982767180ffc01a1e8afc3
shubhya94 <66956873+shubhya94@users.noreply.github.com>
2020-07-25 17:00:53 +0530

    Add files via upload

---
+++ day6 assignment./q1.js
@@ -0,0 +1,15 @@
+
+  var c = ["blue", "black", "yellow", "red"];
+
+  function changeColor() {
+    setTimeout(function loop() {
+      document.bgColor = c.shift();
+      if (c.length) {
+        setTimeout(loop, 5000);
+      }
+    }, 5000);
+  }
+
+  function start() {
+    setInterval(changeColor(), 20000);
+  }
